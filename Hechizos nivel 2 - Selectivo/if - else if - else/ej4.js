6fd816692c7e29d7171786c69c96efa7ce413af9
pc 1 <>
2024-04-19 11:42:48 -0300

    ejercicio 4 realizado

--- Hechizos nivel 2 - Selectivo/if - else if - else/ej4.js
+++ Hechizos nivel 2 - Selectivo/if - else if - else/ej4.js
@@ -5,12 +5,27 @@ const leer = require("prompt-sync")();
  * Verificación de Varita Mágica:
  * Pide al usuario el nivel de lealtad y astucia de una varita mágica y verifica si es adecuada para su uso.
  * A partir de 70 nivel de lealtad o 80 en astucia la varita es apta
- * 
- * Por ejemplo si la astucia es 90 y la lealtad es 40 entonces la varita es apta, en otro caso si la astucia es 30 y la lealtad es 60 la varita no es apta
+ * Por ejemplo si la astucia es 90 y la lealtad es 40 entonces la varita es apta.
+ *  en otro caso si la astucia es 30 y la lealtad es 60 la varita no es apta
  */
 
 
 function main() {
+    let nivelDelealtad = 0
+    let nivelDeastucia = 0
+    console.log("cual es el nivel actual de lealtad en tu varita?");
+    nivelDelealtad = leer()
+    console.log("y cual es el nivel de astucia");
+    nivelDeastucia = leer ()
+
+    if ((nivelDelealtad >=70) || (nivelDeastucia >=80))  {
+
+        console.log("tu varita es apta para darle uso");
+        
+    }
+    else {console.log("tu varita no es apta para usar"); }
+
+
     
 }
 
