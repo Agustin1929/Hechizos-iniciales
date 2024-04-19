c75dd2da766b08effcb43ef34a13c5f030c3fe45
pc 1 <>
2024-04-19 11:22:10 -0300

    ejercicio 3 realizado

--- Hechizos nivel 2 - Selectivo/if - else if - else/ej3.js
+++ Hechizos nivel 2 - Selectivo/if - else if - else/ej3.js
@@ -7,14 +7,31 @@ const leer = require("prompt-sync")();
  * Si es Mandrágora o Valeriana entonces son aptas
  * Si son Bubotuber o Whomping Willow es necesario informar que no es posible debido a lo peligrosas que son
  * Cualquier otro tipo de planta no sera aceptada
- * 
- * Por ejemplo si el estudiante quiere traer un Whomping Willow no podra hacer el examen, en otro caso si trae una Valeriana se le otorgara el permiso
+ * Por ejemplo si el estudiante quiere traer un Whomping Willow no podra hacer el examen.
+ * en otro caso si trae una Valeriana se le otorgara el permiso
  */
 
 
 function main() {
+    let nombreDeplanta = "plantaEncontrada"
+    console.log("que tipo de planta encontraste?");
+    nombreDeplanta = leer();
+
+    if ((nombreDeplanta == "mandragora") || (nombreDeplanta == "valeriana")) {
+
+        console.log("tu planta es apta para el examen");
+    }
+
+    else if ((nombreDeplanta == "bobotuber") || (nombreDeplanta == "whomping willow")) {
+
+        console.log("tu planta no es apta para el examen,es peligrosa");
+
+    } else {console.log("tu tipo de planta no es aceptada,debe ser una de las indicadas en la consigna");}
     
+
+
 }
+
 
 
 main();
