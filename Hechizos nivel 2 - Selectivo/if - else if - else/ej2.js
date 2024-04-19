c038e31dded66ea4e26b5a136a20df515ca01238
pc 1 <>
2024-04-19 10:49:33 -0300

    ejercicio 2 terminado

--- Hechizos nivel 2 - Selectivo/if - else if - else/ej2.js
+++ Hechizos nivel 2 - Selectivo/if - else if - else/ej2.js
@@ -8,14 +8,45 @@ const leer = require("prompt-sync")();
  * Hufflepuff - a partir de 70 puntos
  * Ravenclaw - a partir de 50 puntos
  * Slytherin - menos de 50 puntos
- * 
- * Por ejemplo si el estudiante saco 100 puntos entonces pertenece a Gryffindor o si sacara 30 entonces pertenece a Slytherin
+ * Por ejemplo si el estudiante saco 100 puntos entonces pertenece a Gryffindor.
+ *  o si sacara 30 entonces pertenece a Slytherin
  */
 
 
 function main() {
-    
+    let calificacionFinalexamen
+    console.log("cual fue tu calificacion en el examen");
+    calificacionFinalexamen = Number(leer())
+
+    if (calificacionFinalexamen >= 90) {
+        console.log("ingresaste a la casa gryffindor");
+
+    }
+
+    else if (calificacionFinalexamen >= 70) {
+
+        console.log("ingresaste a la casa hufflepuff");
+
+
+
+    }
+
+    else if (calificacionFinalexamen >= 50) {
+
+        console.log("ingresaste a la casa Ravenclaw");
+
+    }
+
+    else if (calificacionFinalexamen <= 50) {
+
+        console.log("ingresaste a la casa Slytherin");
+
+    }
+
+
 }
+
+
 
 
 main();
