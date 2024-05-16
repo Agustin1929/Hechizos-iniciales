4686340d015c90bf083bfc2ec0371d5984bac61e
pc 0 <>
2024-05-16 09:49:08 -0300

    ejercicio 1 realizado

--- Hechizos nivel 3 - Iterativo/1 - for/ej1.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej1.js
@@ -10,9 +10,40 @@ const leer = require("prompt-sync")();
  * Extra: permitir que el programa acepte 3 hechizos diferentes
  */
 
-
+const TURNOS_TOTALES = 4
+const NOMBRE_HECHIZO_UNO = "windgardium leviosa"
+const NOMBRE_HECHIZO_DOS = "avada kedavra"
+const NOMBRE_HECHIZO_TRES = "patronus"
 function main() {
-    
+    let nombreEchizo
+    let turnos = TURNOS_TOTALES
+    for (let i = 0; i < 4; i++) {
+        console.log("cual es el nombre del echizo que deseas lanzar?");
+        nombreEchizo = leer()
+        if (nombreEchizo == NOMBRE_HECHIZO_UNO) {
+            console.log("BIEN! lanzaste el echizo windgardium leviosa");
+            turnos = turnos - 1
+            console.log("te quedan en total \t", turnos, "turnos");
+        }
+        else if (nombreEchizo == NOMBRE_HECHIZO_DOS) {
+            console.log("BIEN! lanzaste el echizo avada kedavra");
+            turnos = turnos - 1
+            console.log("te quedan en total \t", turnos, "turnos");
+        }
+        else if (nombreEchizo == NOMBRE_HECHIZO_TRES) {
+            console.log("BIEN! lanzaste el echizo patronus");
+            turnos = turnos - 1
+            console.log("te quedan en total \t", turnos, "turnos");
+        }
+        else { console.log("hechizo incorrecto!! \t volve a intentarlo");
+               turnos = turnos - 1
+               console.log("te quedan en total \t", turnos, "turnos");
+        }
+    }
+
+
+
+
 }
 
 
